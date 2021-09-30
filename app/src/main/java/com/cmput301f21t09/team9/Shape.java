7e137a6a110aabf362239af12b71b3f8163c98d7
Mac Malainey <mmalaine@ualberta.ca>
2021-09-30 09:18:41 -0600

    Add "color" field to Shape

--- app/src/main/java/com/cmput301f21t09/team9/Shape.java
+++ app/src/main/java/com/cmput301f21t09/team9/Shape.java
@@ -3,6 +3,7 @@ package com.cmput301f21t09.team9;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "$YOUR_COLOR_OF_CHOICE";
 
     Shape(int x, int y) {
         this.x = x;
