a998d4f170d98a0c7e4b9a84c11577f98eb5ef09
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-09-30 10:19:16 -0600

    Add color attribute

--- app/src/main/java/com/cmput301f21t09/team9/Shape.java
+++ app/src/main/java/com/cmput301f21t09/team9/Shape.java
@@ -3,6 +3,7 @@ package com.cmput301f21t09.team9;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "maroon";
 
     Shape(int x, int y) {
         this.x = x;
